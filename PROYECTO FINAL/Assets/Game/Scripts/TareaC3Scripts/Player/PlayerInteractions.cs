47684b0243220feb7a6a50abe00f6296d45ddafb
Santiago <santiago.munoz_tro@uao.edu.co>
2025-10-27 12:42:42 -0500

    Avance Bases Proyecto Escena 3 Munoz

---
+++ PROYECTO FINAL/Assets/Game/Scripts/TareaC3Scripts/Player/PlayerInteractions.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    public Transform startPosition;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GunAmmo"))
+        {
+            GameManager.Instance.gunAmmo += other.gameObject.GetComponent<AmmoBox>().ammo;
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("HealthObject"))
+        {
+            GameManager.Instance.AddHealth(other.GetComponent<HealthObject>().health);
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("DeathFloor"))
+        {
+            GameManager.Instance.LoseHealth(20);
+
+            GetComponent<CharacterController>().enabled = false;
+            gameObject.transform.position = startPosition.position;
+            GetComponent<CharacterController>().enabled = true;
+
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyBullet"))
+        {
+            GameManager.Instance.LoseHealth(5);
+        }
+    }
+}
+    
+
+
